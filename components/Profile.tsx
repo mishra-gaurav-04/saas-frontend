c4dabf604092d6c0eea280ea191928aa272ab93b
Prateek Parmar <prateekparmar2003@gmail.com>
2024-02-19 23:28:56 +0530

    fixed css errors

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -6,28 +6,28 @@ const Profile = () => {
   return (
     <>
       <div className="top-0 h-screen w-[25%] bg-white p-4">
-        <form class="flex items-center max-w-sm mx-auto mb-4">
-          <label for="simple-search" class="sr-only">
+        <form className="flex items-center max-w-sm mx-auto mb-4">
+          <label className="sr-only">
             Search
           </label>
-          <div class="relative w-full">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <div className="relative w-full">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
               <FaSearch />
             </div>
             <input
               type="text"
               id="simple-search"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Search..."
               required
             />
           </div>
           <button
             type="submit"
-            class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <FaSearch />
-            <span class="sr-only">Search</span>
+            <span className="sr-only">Search</span>
           </button>
         </form>
         <div className="text-center border-b-2 pb-4">
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,16 +4,13 @@ import { FaHome } from "react-icons/fa";
 import { PiChats } from "react-icons/pi";
 import { TbBulb } from "react-icons/tb";
 import { IoIosLogOut } from "react-icons/io";
-import { RxHamburgerMenu } from "react-icons/rx";
 
 const Sidebar = () => {
   return (
     <>
-      
-
       <aside
         id="default-sidebar"
-        className="top-0 left-0 z-40 w-auto h-screen flex flex-col justify-between overflow-y-auto bg-gray-50 dark:bg-gray-800"
+        className="top-0 left-0 z-40 w-auto h-screen flex flex-col justify-between overflow-y-auto bg-[#6656FF] dark:bg-gray-800"
         aria-label="Sidebar"
       >
         <div className="px-3 py-4 overflow-y-auto">
@@ -32,8 +29,8 @@ const Sidebar = () => {
                 href="#"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <FaHome className="text-2xl" />
-                <span className="ms-3">My Ai</span>
+                <FaHome className="text-2xl text-white" />
+                <span className="ms-3 text-white">My Ai</span>
               </a>
             </li>
             <li>
@@ -41,8 +38,10 @@ const Sidebar = () => {
                 href="#"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <PiChats className="text-2xl" />
-                <span className="flex-1 ms-3 whitespace-nowrap">Chats</span>
+                <PiChats className="text-2xl text-white" />
+                <span className="flex-1 ms-3 whitespace-nowrap text-white">
+                  Chats
+                </span>
               </a>
             </li>
             <li>
@@ -50,8 +49,8 @@ const Sidebar = () => {
                 href="#"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <TbBulb className="text-2xl" />
-                <span className="flex-1 ms-3 whitespace-nowrap">
+                <TbBulb className="text-2xl text-white" />
+                <span className="flex-1 ms-3 whitespace-nowrap text-white">
                   Preferences
                 </span>
               </a>
@@ -65,8 +64,10 @@ const Sidebar = () => {
                 href="#"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
-                <IoIosLogOut />
-                <span className="flex-1 ms-3 whitespace-nowrap">LogOut</span>
+                <IoIosLogOut className="text-2xl text-white" />
+                <span className="flex-1 ms-3 whitespace-nowrap text-white">
+                  LogOut
+                </span>
               </a>
             </li>
           </ul>
