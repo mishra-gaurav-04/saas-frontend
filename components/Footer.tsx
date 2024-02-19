91865045c3bc68122926e7ca8e813bf8ab949560
Gaurav Mishra <gaurav.info217@gmail.com>
2024-02-19 23:15:48 +0530

    updated landing page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='bg-[#6656FF] w-full h-screen'>
-        <div className='container'>
-
+        <div className='container mx-auto'>
+            
         </div>
     </footer>
   )
