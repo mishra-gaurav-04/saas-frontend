c173ce9d6f6766e89aac7e6da6fd3739aef8c157
Prateek Parmar <prateekparmar2003@gmail.com>
2024-02-29 22:21:29 +0530

    added routes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -26,7 +26,7 @@ const Sidebar = () => {
           <ul className="space-y-2 font-medium">
             <li>
               <a
-                href="#"
+                href="/dashboard"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#493dbc] dark:hover:bg-gray-700 group"
               >
                 <FaHome className="text-2xl text-white" />
@@ -35,7 +35,7 @@ const Sidebar = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="/chat"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#493dbc] dark:hover:bg-gray-700 group"
               >
                 <PiChats className="text-2xl text-white" />
@@ -46,7 +46,7 @@ const Sidebar = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="/preferences"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#493dbc] dark:hover:bg-gray-700 group"
               >
                 <TbBulb className="text-2xl text-white" />
