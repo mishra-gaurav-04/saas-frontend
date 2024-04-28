8bb25a54f915bda2c5ff67c5b88bf3b615f81a14
Prateek Parmar <prateekparmar2003@gmail.com>
2024-04-29 00:34:21 +0530

    posting answers to the answer route

--- components/Preferences.tsx
+++ components/Preferences.tsx
@@ -4,13 +4,11 @@ import { HiOutlineMenu } from "react-icons/hi";
 import { useEffect, useState, useRef } from "react";
 import { useSession } from "next-auth/react";
 
-interface Answer {
-  [key: string]: string;
-}
+
 
 const Preferences = ({ toggle, setToggle }: any) => {
   const [questions, setQuestions] = useState([]);
-  const [answers, setAnswers] = useState({});
+  const [answers, setAnswers] = useState<{ questionId: string; text: string; }[]>([]);
   const effect = useRef(false);
   const { data: session, status } = useSession();
 
@@ -39,18 +37,19 @@ const Preferences = ({ toggle, setToggle }: any) => {
   // console.log("Questions\n", questions);
 
   const handleInputChange = (e: any) => {
-    setAnswers((prev) => ({
-      ...prev,
-      [e.target.id]: e.target.value,
-    }));
+    const newAnswer = {
+      questionId: e.target.id,
+      text: e.target.value,
+    };
+    setAnswers((prev) => [...prev, newAnswer]);
   };
 
   console.log("This is answers\n", answers);
 
   // console.log("Prefrence\n", session?.user?.id);
 
-  const handleSubmit = async (e:any) => {
-    e.preventDefault()
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
     try {
       const response = await fetch(
         `http://127.0.0.1:5000/api/answer/${session?.user?.id}`,
@@ -73,11 +72,6 @@ const Preferences = ({ toggle, setToggle }: any) => {
       // Handle error, e.g., show an error message
     }
   };
-
-  // const handleLaunchClick = () => {
-  //   const userId = session?.user?.id; // Set the user ID here or fetch it from somewhere
-  //   // handleSubmit(userId);
-  // };
 
   return (
     <>
@@ -107,7 +101,10 @@ const Preferences = ({ toggle, setToggle }: any) => {
           </button>
         </div>
         <div className="flex flex-col h-[90%] items-center justify-center w-full">
-          <form onSubmit={handleSubmit} className="w-[80%] max-h-[90%] bg-white p-4 rounded-xl shadow-2xl border-2 overflow-scroll">
+          <form
+            onSubmit={handleSubmit}
+            className="w-[80%] max-h-[90%] bg-white p-4 rounded-xl shadow-2xl border-2 overflow-scroll"
+          >
             {questions.map((item: any) => (
               <div className="relative z-0 w-full mb-5 group">
                 <input
@@ -117,7 +114,7 @@ const Preferences = ({ toggle, setToggle }: any) => {
                   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
-                  onChange={(e) => handleInputChange(e)}
+                  onBlur={(e) => handleInputChange(e)}
                 />
                 <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                   {item.title}
