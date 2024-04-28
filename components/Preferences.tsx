04d3890061fb98c9c7580478f2ec8568637a5b1a
Gaurav <gaurav.info217@gmail.com>
2024-04-28 22:11:46 +0530

    fix prefrences

--- components/Preferences.tsx
+++ components/Preferences.tsx
@@ -46,7 +46,7 @@ const Preferences = ({ toggle, setToggle }: any) => {
   const handleInputChange = (e: any) => {
     setAnswers((prev) => ({
       ...prev,
-      [e.target.name]: e.target.value,
+      [e.target.id]: e.target.value,
     }));
   };
 
@@ -113,11 +113,10 @@ const Preferences = ({ toggle, setToggle }: any) => {
                 <input
                   type="text"
                   name={item._id.$oid}
-                  id={`question-${item._id.$oid}`}
+                  id={item._id.$oid}
                   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
-                  value={answers[item._id.$oid] as Answer || ""}
                   onChange={(e) => handleInputChange(e)}
                 /> 
                 <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
